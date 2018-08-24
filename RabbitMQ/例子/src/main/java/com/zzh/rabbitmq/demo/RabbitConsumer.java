d0885e00c6285bbb7924126b7a053202f591dcbc
laputa <laputa@laputa-PC>
2018-08-24 17:43:58 +0800

    1

---
+++ RabbitMQ/例子/src/main/java/com/zzh/rabbitmq/demo/RabbitConsumer.java
@@ -0,0 +1,76 @@
+package com.zzh.rabbitmq.demo;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Address;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class RabbitConsumer {
+	private static final String QUEUE_NAME = "queue_demo";
+	private static final String IP_ADDRESS = "localhost";
+	private static final int PORT = 5672;
+	
+	public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+		Address[] addresses = new Address[] {
+				new Address(IP_ADDRESS, PORT)
+		};
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUsername("root");
+		factory.setPassword("root");
+		
+		// 这里的连接方式与生产者的demo略有不同，注意辨别区别
+		Connection connection = factory.newConnection(addresses); // 创建连接
+		final Channel channel = connection.createChannel(); // 创建信道
+		channel.basicQos(64); // 设置客户端最多接收未被 ack 的消息个数
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+				System.out.println("recv message: " + new String(body));
+				
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch ( InterruptedException e ) {
+					e.printStackTrace();
+				}
+				channel.basicAck(envelope.getDeliveryTag(), false);
+			}
+		};
+		
+		channel.basicConsume(QUEUE_NAME, consumer);
+		
+		// 等待回调函数执行完毕之后，关闭资源
+		TimeUnit.SECONDS.sleep(5);
+		channel.close();
+		connection.close();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
